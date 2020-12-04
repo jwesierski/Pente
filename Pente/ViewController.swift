e7f172caaf3a3388595a540b939b0f92c13a266d
jwesierski <wesierskij@gmail.com>
2020-12-03 19:10:48 -0800

    Pente app outline

--- Pente/ViewController.swift
+++ Pente/ViewController.swift
@@ -4,7 +4,45 @@
 //
 //  Created by Jack Michael Wesierski on 11/10/20.
 //
+/*
+ //pull from week of 11/12
+ You are required to create a functional App with the following features (minimum requirement 75 - 85):
 
+ 3-5 screens connected through Navigation Controller or Tab View Controller
+    Home Screen-name and catchphrase
+    Screen 2-All languages
+        (inline section)(main menu) (pickerview)
+    Screen 3-All learning activities
+        French Optional second Screen 3 Spanish
+        *lots of design and media stuff)
+        Categories (Segmented Control)
+        Each control has its own picker view
+        (Movie clips) (Youtube videos) (music videos) (sounds clips)- *Either just text or text with images for each category and or s
+            Whichever is the easiest to pull/ embed and have a (running?) translation
+        picker view
+        video clips with tranlations (take from the internet or download)
+        Music videos potentially
+        Songs
+    Screen 4-Individual learning acitivity
+        Sound or clip plays at the top of the screen
+        translation below
+        Start with just pasted translated text then focus on realtime translations
+        //https://stackoverflow.com/questions/35006738/auto-scroll-for-uitextview-using-swift-ios-app         potentially dubbed videos or videos with translations on screen. 
+ 
+ Include: objects such as images, labels, buttons, switches, text views
+ All the elements should be layout appropriately through constraints or stack view
+ Your app should serve a purpose-
+    For people to learn language through media translation
+
+ More advanced features (85 - 100):
+ Table Views
+ Segues
+ JSON Parsing through API
+ Mapkit
+
+ */
+  
+import UIKit
 import UIKit
 
 class ViewController: UIViewController {
