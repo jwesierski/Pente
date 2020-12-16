6f9a6c140109be6651131017da0f19df1845a185
jwesierski <wesierskij@gmail.com>
2020-12-15 22:56:19 -0800

    committed sound to tableViewController. Displaying scrolling text and playing la vie en rose with a push  of the same button (:

--- Pente/TableViewController.swift
+++ Pente/TableViewController.swift
@@ -8,119 +8,29 @@
 import UIKit
 import SwiftUI
 import AVKit
+import AVFoundation
 
 class TableViewController: UITableViewController {
-    
+    var audioPlayer: AVAudioPlayer!
     @IBOutlet weak var scrollView: UIScrollView!
     
     @IBAction func hitBtn(_ sender: Any) {
         UIScrollView.animate(withDuration: 15, animations: {
         self.scrollView.contentOffset = CGPoint(x: 0, y: 1200) })
+        if let soundURL = Bundle.main.url(forResource: "frenchSongTest", withExtension: "mp3") {
+                
+                    do {
+                        audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+                    }
+                    catch {
+                        print(error)
+                    }
+                    
+                    audioPlayer.play()
+                }else{
+                    print("Unable to locate audio file")
+                }
     }
     
-    
-
-/*
-    struct ContentView: View {
-        var body: some View {
-            VStack {
-                player().frame(height: UIScreen.main.bounds.height / 3)
-                Spacer()
-            }
-        }
-    }
-
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
-    }
-
-    struct player : UIViewControllerRepresentable {
-        
-        func makeUIViewController(context: UIViewControllerRepresentableContext<player>) -> AVPlayerViewController{
-            let controller = AVPlayerViewController()
-            let url = "https://www.youtube.com/watch?v=lrlQR_KH_nw"
-            let player1 = AVPlayer(url: URL(string: url)!)
-            //the code had me add the exclamation at the end of line 31 in case the value was null. I believe it's becuase of the url link and I copied the url instead of the video link. See if that can be changed
-            controller.player = player1
-            return controller
-        }
-        func updateUIViewController(_ uiViewController: AVPlayerViewController, context: UIViewControllerRepresentableContext<player>) {
-        
-        }
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-      
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-*/
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
